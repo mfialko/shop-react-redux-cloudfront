60d18073cd63882cc8fbe0367dd48cfb95ddf050
Mykhailo Fialko <Mykhailo_Fialko@epam.com>
2023-10-09 17:05:57 +0300

    Task-3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,12 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  productList:
+    "https://d2gkvboy0z6tav.lambda-url.eu-central-1.on.aws/",
+  product:
+    "https://d2gkvboy0z6tav.lambda-url.eu-central-1.on.aws/",
+  order: "https://d2gkvboy0z6tav.execute-api.eu-central-1.amazonaws.com/dev/",
+  import: "https://d2gkvboy0z6tav.execute-api.eu-central-1.amazonaws.com/dev/",
+  bff: "https://d2gkvboy0z6tav.execute-api.eu-central-1.amazonaws.com/dev/",
+  cart: "https://d2gkvboy0z6tav.execute-api.eu-central-1.amazonaws.com/dev/",
 };
 
 export default API_PATHS;
