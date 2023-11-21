992a32485d261f5fbe5449e4da52e8a6f5a9de2d
Mykhailo Fialko <Mykhailo_Fialko@epam.com>
2023-11-21 20:47:36 +0200

    moved be to separate repository

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,10 +1,11 @@
+const BASE_URL = "https://qskztss3t1.execute-api.eu-west-1.amazonaws.com/dev";
+
 const API_PATHS = {
-  products:
-    "https://rdhb7vkhp7.execute-api.eu-west-1.amazonaws.com/dev/products",
-  order: "https://d2gkvboy0z6tav.execute-api.eu-central-1.amazonaws.com/dev/",
-  import: "https://d2gkvboy0z6tav.execute-api.eu-central-1.amazonaws.com/dev/",
-  bff: "https://d2gkvboy0z6tav.execute-api.eu-central-1.amazonaws.com/dev/",
-  cart: "https://d2gkvboy0z6tav.execute-api.eu-central-1.amazonaws.com/dev/",
+  products: `${BASE_URL}/products`,
+  order: `${BASE_URL}`,
+  import: `${BASE_URL}`,
+  bff: `${BASE_URL}`,
+  cart: `${BASE_URL}`,
 };
 
 export default API_PATHS;
