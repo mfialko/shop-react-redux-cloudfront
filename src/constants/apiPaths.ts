cf5247e52826b349bffe8f772beec4883442438b
Mykhailo Fialko <Mykhailo_Fialko@epam.com>
2023-11-27 18:50:41 +0200

    task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
-const BASE_URL = "https://qskztss3t1.execute-api.eu-west-1.amazonaws.com/dev";
+const BASE_URL = "https://r4x7ybtgm8.execute-api.eu-west-1.amazonaws.com/dev";
 
 const API_PATHS = {
   products: `${BASE_URL}/products`,
   order: `${BASE_URL}`,
-  import: `${BASE_URL}`,
+  import: `${BASE_URL}/import`,
   bff: `${BASE_URL}`,
   cart: `${BASE_URL}`,
 };
